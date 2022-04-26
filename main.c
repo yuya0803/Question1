63e202266d67cfbaab151195599993cd14abf59e
yuya <o.y.02386@gmail.com>
2022-04-26 18:10:05 +0900

    error1

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "calc.h"
+
+int main()
+{
+    int n,k,sum;
+
+    printf("問1 (1) n=");
+    scanf("%d",&n);
+
+    sum1=sigmakk(k,n);
+
+    printf("問1 (1) n=%dの時=%d\n",n,sum1);
+
+    return 0;
+}
